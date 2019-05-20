716458b54073a0e792cb893ccaf04b9960394e4c
于剑楠 <yujiannan@megvii.com>
2019-05-20 10:30:18 +0800

    安装fastdtw包

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from util import get_data0_data
 
 DEBUG = False
 
+
+def convert_data0_to_gait_cycles_file(people_index: int):
+    """
+    把data0的数据进行周期分割，然后存进文件里供后续CNN等使用
+    :param people_index:
+    :return:
+    """
+
+
 if __name__ == "__main__":
     if not DEBUG:
         GuiManager().run()
@@ -16,4 +25,5 @@ if __name__ == "__main__":
     algorithm_manager.gyro_data_pre_process.DEBUG = True
     while True:
         sensor_manager.mock_real_time_data_from_data0()
-        sensor_manager.gyro, cycle = algorithm_manager.gyro_data_pre_process.get_gait_cycle(sensor_manager.gyro)
+        sensor_manager.gyro, cycle = algorithm_manager.gyro_data_pre_process.get_gait_cycle(
+            sensor_manager.gyro)
